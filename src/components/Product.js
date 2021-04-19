7743f05efe01be9b2fc5d1a54d123d13e688228c
Pin Jui <fee8ack21@gmail.com>
2021-04-19 21:13:56 +0800

    Chap2 finished

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { formatPrice } from "common/helper";
 class Product extends React.Component {
   render() {
+    const { name, image, tags, price, available } = this.props.product;
+
     return (
-      <div className="product col-3">
-        <p>Product</p>
+      <div className="product col-3 mb-3">
+        <div className="bg-white rounded p-3">
+          <div>
+            <figure className="position-relative rounded">
+              <div
+                class="position-absolute"
+                style={{
+                  top: "0px",
+                  left: "0px",
+                  right: "0px",
+                  bottom: "0px",
+                  backgroundColor: "rgba(0,0,0,0.7)",
+                }}
+              >
+                {available ? "" : "Out of Stock"}
+              </div>
+              <img className="img-fluid" src={image} alt="" />
+            </figure>
+            <small>{this.props.tags}</small>
+            <p>{name}</p>
+          </div>
+          <div className="d-flex justify-content-between align-items-center">
+            <p className="mb-1">{formatPrice(price)}</p>
+            <button
+              className="position-relative btn bg-secondary rounded-circle"
+              style={{ width: "32px", height: "32px" }}
+            >
+              <i
+                class="fas fa-shopping-cart position-absolute"
+                style={{
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%,-50%)",
+                }}
+              ></i>
+              {/* <i class="fas fa-exclamation"></i> */}
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,18 +2,36 @@ import React from "react";
 // import ReactDOM from "react-dom";
 import ProductToolBox from "components/ProductToolBox";
 import Product from "components/Product";
+import { NavItem } from "react-bootstrap";
 // 函式寫法
 function Products() {
+  const product = [
+    {
+      id: "1",
+      name: "air Jordan 4",
+      price: "1234",
+      tags: "test",
+      image: "images/1.jpeg",
+      available: true,
+    },
+    {
+      id: "2",
+      name: "air Jordan 4",
+      price: "1214",
+      tags: "test",
+      image: "images/1.jpeg",
+      available: false,
+    },
+  ];
   return (
     <>
       <div className="container">
         <ProductToolBox />
-        <div className="product-list row">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+        <div className="product-list row mt-3">
+          {/* <Product product={product} /> */}
+          {product.map((item) => {
+            return <Product product={item} key={item.id} />;
+          })}
         </div>
       </div>
     </>
