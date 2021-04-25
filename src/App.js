0e891ba174c7e2cc787d9239119fc2c15eb31a9b
Pin Jui <fee8ack21@gmail.com>
2021-04-25 17:01:51 +0800

    chat plugin debug

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "scss/app.scss";
 import Router from "Router";
-import MessengerCustomerChat from "react-messenger-customer-chat";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
 //
 class App extends React.Component {
   render() {
     return (
       <>
         <Router />
-        <MessengerCustomerChat
+        {/* <MessengerCustomerChat
           pageId="104903445063158"
           appId="366104244735566"
-          // htmlRef={window.location.pathname}
-        />
+          htmlRef={window.location.pathname}
+        /> */}
       </>
     );
   }
 }
-// 
+//
 export default App;
