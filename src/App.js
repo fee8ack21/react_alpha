77603eee15919631886d1952a40cdab49b35553c
Pin Jui <fee8ack21@gmail.com>
2021-04-25 17:14:41 +0800

    adjust package json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "scss/app.scss";
 import Router from "Router";
-// import MessengerCustomerChat from "react-messenger-customer-chat";
 //
 class App extends React.Component {
   render() {
     return (
       <>
         <Router />
-        {/* <MessengerCustomerChat
-          pageId="104903445063158"
-          appId="366104244735566"
-          htmlRef={window.location.pathname}
-        /> */}
       </>
     );
   }
